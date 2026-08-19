cb2f276a7fa107f648f32c5fd439f00d782da428
talent25hunt <talent.25.hunt@gmail.com>
2026-08-19 16:11:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+const reveals = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+
+  reveals.forEach((el) => {
+
+    const top = el.getBoundingClientRect().top;
+
+    if (top < window.innerHeight - 100) {
+      el.classList.add('active');
+    }
+
+  });
+
+});
